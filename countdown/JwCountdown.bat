eae47fb20d3fcd0d565bd8e8e1b28598618169ea
1Dkvr <1D.KVR.user@gmail.com>
2026-09-18 12:52:41 +0200

    Fix license URL in JwCountdown.bat
    Updated license URL in the JwCountdown.bat file.

--- countdown/JwCountdown.bat
+++ countdown/JwCountdown.bat
@@ -13,7 +13,7 @@
 :: Runtime     : Windows PowerShell 5.1+
 :: Platform    : Microsoft Windows
 :: License     : Custom Non-Commercial Source-Available
-:: License URL : https://github.com/1Dkvr/LICENSE.md
+:: License URL : https://github.com/1Dkvr/jw/blob/main/LICENSE.md
 ::
 :: This source code is subject to the terms defined in the `LICENSE.md` file located in the root of this repository or online at the URL above.
 ::
