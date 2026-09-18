b026aa0af8e6b88da5bdc930f2c7ea2e965a0749
1Dkvr <1D.KVR.user@gmail.com>
2026-09-18 11:30:07 +0200

    Revise JwCountdown.bat script
    Updated the script header and corrected the start command.

--- countdown/JwCountdown.bat
+++ countdown/JwCountdown.bat
@@ -7,8 +7,7 @@
 :: Author      : 1Dkvr
 :: Copyright   : © 2026 1Dkvr. All rights reserved.
 :: Created     : 26.09.01
-:: Version     : 26.09.15
-:: Description : Launches the JW Countdown launcher and update manager.
+:: Description : Starts the JW Countdown launcher and update manager.
 :: File        : JwCountdown.bat
 :: Runtime     : Windows PowerShell 5.1+
 :: Platform    : Microsoft Windows
@@ -16,4 +15,6 @@
 ::
 :: ============================================================================
 
-START /B powershell.exe -ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -File "%~dp0JwCountdownLauncher.ps1"
+START "" /B powershell.exe -ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -File "%~dp0JwCountdownLauncher.ps1"
+
+exit /b
