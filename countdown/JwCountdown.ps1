df5a4c3d2a1550fc1af7b12966822a795299dfb8
1Dkvr <1D.KVR.user@gmail.com>
2026-09-18 12:54:42 +0200

    Update JwCountdown.ps1

--- countdown/JwCountdown.ps1
+++ countdown/JwCountdown.ps1
@@ -60,16 +60,18 @@
     Component    : Main application
     Created      : 26.09.01
     Developer    : 1Dkvr
+    Publisher    : Hold'inCorp.
     Platform     : Microsoft Windows
     Runtime      : Windows PowerShell 5.1+
     Dependencies : System.Windows.Forms, System.Drawing
-    License      : Proprietary
+    License      : Custom Non-Commercial Source-Available
+    License URL  : https://github.com/1Dkvr/jw/blob/main/LICENSE.md
 
-    Copyright © 2026 1Dkvr. All rights reserved.
+    Copyright © 2026 [Publisher]. All rights reserved.
 
-    This software and its source code are proprietary and protected by applicable intellectual property laws.
-
-    Unauthorized copying, modification, distribution, publication, sublicensing or commercial use is prohibited without prior authorization from the copyright holder.
+    This source code is subject to the terms and conditions defined in 
+    the 'LICENSE.md' file located in the root directory of this repository 
+    or online at [License URL].
 
     No external module or third-party dependency is required.
 
