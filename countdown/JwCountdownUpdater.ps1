bf7b3437381d248a8c452753fb29a7ee688330a0
1Dkvr <1D.KVR.user@gmail.com>
2026-09-21 12:50:41 +0200

    Update creation date and copyright years in JwCountdownUpdater

--- countdown/JwCountdownUpdater.ps1
+++ countdown/JwCountdownUpdater.ps1
@@ -31,7 +31,7 @@
 .NOTES
     Product      : JW Countdown
     Component    : Update installer
-    Created      : 26.09.01
+    Created      : 19.09.01
     Developer    : 1Dkvr
     Licensor     : Hold'inCorp.
     Platform     : Microsoft Windows
@@ -40,7 +40,7 @@
     License      : Custom Non-Commercial Source-Available
     License URL  : https://github.com/1Dkvr/jw/blob/main/LICENSE.md
 
-    Copyright © 2026 [Licensor]. All rights reserved.
+    Copyright © 2019-2027 [Licensor]. All rights reserved.
 
     This source code is subject to the terms and conditions defined in the 'LICENSE.md' file located in the root directory of this repository or online at the URL above.
     No external module or third-party dependency is required.
