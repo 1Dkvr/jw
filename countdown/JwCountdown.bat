f9634b91273c81abbd9f66ccded20d9a96a37fde
1Dkvr <1D.KVR.user@gmail.com>
2026-09-21 12:48:37 +0200

    Revise copyright and author details in JwCountdown.bat
    Updated copyright year and author information.

--- countdown/JwCountdown.bat
+++ countdown/JwCountdown.bat
@@ -6,8 +6,7 @@
 ::
 :: Author      : 1Dkvr
 :: Licensor    : Hold'inCorp.
-:: Copyright   : © 2026 Hold'inCorp. All rights reserved.
-:: Created     : 26.09.01
+:: Created     : 19.09.01
 :: Description : Starts the JW Countdown launcher and update manager.
 :: File        : JwCountdown.bat
 :: Runtime     : Windows PowerShell 5.1+
@@ -15,7 +14,7 @@
 :: License     : Custom Non-Commercial Source-Available
 :: License URL : https://github.com/1Dkvr/jw/blob/main/LICENSE.md
 ::
-:: Copyright © 2026 [Licensor]. All rights reserved.
+:: Copyright © 2019-2027 [Licensor]. All rights reserved.
 ::
 :: This source code is subject to the terms and conditions defined in the 'LICENSE.md' file located in the root directory of this repository or online at at the URL above.
 ::
